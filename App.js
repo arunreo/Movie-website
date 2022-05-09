ea13a299712ddc2a681f37204bed290bb4c83d43
arunreo <101916212+arunreo@users.noreply.github.com>
2022-05-09 21:15:37 +0530

    files added

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './Assigenmnt-2/Footer';
+import Home1 from './Assigenmnt-2/Home';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Home1/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
