9d71fecbede5a6f8e5c2a0cc3a0dfd050ebd0fef
arunreo <101916212+arunreo@users.noreply.github.com>
2022-05-10 21:52:20 +0530

    Update Footer.js

--- Assigenmnt-2/Footer.js
+++ Assigenmnt-2/Footer.js
@@ -6,13 +6,13 @@ import React, { Component } from 'react'
       <div className="footercontent">
           <div className="connect">
     
-            <p >  crizmovies@kerala.com</p>
+            <p > arunmovies@gmail.com</p>
             <span className="fa-stack fa-lg">
                 <i className="fa fa-circle-thin fa-stack-2x"></i>
                 <i className="fa fa-twitter fa-stack-1x"></i>
             </span>
             © 2021<br />
-            TicketAvail Technologies Private Limited
+            TicketAvailable Technologies Private Limited
         </div>
     </div>
         ) 
